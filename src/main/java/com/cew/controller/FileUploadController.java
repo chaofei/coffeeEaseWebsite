bdf65fa8e0d43b8dc91a048f5ce25d3d3e6cbd93
chenchaofei <chenchaofei@geotmt.com>
2017-03-12 21:42:08 +0800

    file upload

---
+++ src/main/java/com/cew/controller/FileUploadController.java
@@ -0,0 +1,59 @@
+package com.cew.controller;
+
+/**
+ * Created by chenchaofei on 2017/3/12.
+ */
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUploadController {
+
+    @Autowired
+    public Environment env;
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
+                                                 @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            System.out.println("上传的文件名为：" + fileName);
+
+            // 获取文件的后缀名
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            System.out.println("上传的文件后缀名为：" + suffixName);
+
+            // 文件上传路径
+            String destPath = ClassUtils.getDefaultClassLoader().getResource("").getPath() +
+                    env.getProperty("upload.dir") + name + suffixName;
+            File dest = new File(destPath);
+
+            System.out.println();
+
+            // 检测是否存在目录
+            if (!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();
+            }
+
+            try {
+                file.transferTo(dest);
+                return "You successfully uploaded " + fileName + " into " + destPath;
+            } catch (Exception e) {
+                return "You failed to upload " + fileName + ":"+destPath+" => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+    }
+
+}
